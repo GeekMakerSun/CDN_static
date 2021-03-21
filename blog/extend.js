ebbe56baa30e0bc5c8730764421ded42cc71fee1
GeekMakerSun <mail@13aq.com>
2021-03-22 07:00:35 +0800

    修改美洽

--- blog/extend.js
+++ blog/extend.js
@@ -1,1 +1,13 @@
-console.log('测试')
+// 美洽
+(function(a, b, c, d, e, j, s) {
+    a[d] = a[d] || function() {
+        (a[d].a = a[d].a || []).push(arguments)
+    };
+    j = b.createElement(c),
+        s = b.getElementsByTagName(c)[0];
+    j.async = true;
+    j.charset = 'UTF-8';
+    j.src = 'https://static.meiqia.com/widget/loader.js';
+    s.parentNode.insertBefore(j, s);
+})(window, document, 'script', '_MEIQIA');
+_MEIQIA('entId', '4c273ef3141cc5bfe8a1dfb8cef2fdb1');
